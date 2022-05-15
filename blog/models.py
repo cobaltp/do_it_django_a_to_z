b47fb5c4c4c0b667045857ab7f40b1f796371966
kmankim <kmankim.dev@gmail.com>
2022-05-15 18:31:33 +0900

    Added: Preview for Blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     header_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -16,3 +18,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_filename(self):
+        return os.path.basename(self.file_attachment.name)
+    
+    def get_fileext(self):
+        return self.get_filename().split('.')[-1]
